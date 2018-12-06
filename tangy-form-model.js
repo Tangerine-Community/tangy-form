d96d338216e20c34059042c5a6523c91e937fb45
R.J. Steinert <rj@rjsteinert.com>
2018-12-06 16:11:52 -0500

    Remove ununsed tangy-form-model.js

--- tangy-form-model.js
+++
@@ -1,9 +0,0 @@
-export class TangyFormModel {
-  constructor(props) {
-    this._id = '' 
-    this.collection = 'TangyForm'
-    this.formId
-    this.responseId = ''
-    Object.assign(this, props)
-  }
-}
