4c9f177486e9c5fc507f95bd759472c27791cdd7
R.J. Steinert <rj@rjsteinert.com>
2020-01-02 14:42:20 -0500

    Add tangy-form-response-model.d.ts for TangyFormResponseModel typings in TypeScript

---
+++ tangy-form-response-model.d.ts
@@ -0,0 +1,19 @@
+export class TangyFormResponseModel {
+  _id:string
+  _rev:string
+  lastModified:Number
+  collection:string
+  form:any
+  items:Array<any>
+  complete:boolean
+  focusIndex:Number
+  nextFocusIndex:Number
+  previousFocusIndex:Number
+  startDatetime:Number
+  startUnixtime:Number
+  uploadDatetime:Number
+  location:any
+  inputs:Array<any>
+  inputsByName:any
+  constructor(data?:any)
+}
