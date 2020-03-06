89b2358e65788e85a2c475bebc4bf964e52762ba
Chris E. Kelley <ckelley@rti.org>
2020-03-06 15:06:35 -0600

    Removed missing translation logging

--- util/t.js
+++ util/t.js
@@ -3,7 +3,6 @@ export const t = (fragment) => {
   if (window.translation[fragment]) {
     return window.translation[fragment]
   } else {
-    console.warn(`i18n: Translation not found for "${fragment}"`)
     return fragment
   }
 }
