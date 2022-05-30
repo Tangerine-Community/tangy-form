b2f2346ab8856560d2e58392ec83ddd9ec6de68b
Watkins, Rebecca <rwatkins@rti.org>
2022-05-30 19:06:30 -0400

    [#337] Additional space removal.

--- tangy-form-item.js
+++ tangy-form-item.js
@@ -94,7 +94,7 @@ export class TangyFormItem extends PolymerElement {
           margin: 0px
         }
         :host([fullscreen-enabled]) paper-card  {
-          padding-top: 53px;
+          /*padding-top: 53px;*/
           overflow: scroll;
         }
         :host([fullscreen-enabled][fullscreen-nav-align="top"]) .card-actions {
